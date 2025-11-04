e79daba7f32f8185338af4c1d09706d44b7aa1b8
Zixuan Huang <hzx707482606@gmail.com>
2025-11-04 18:23:40 +1100

    Update: commit

--- commit.py
+++ commit.py
@@ -11,23 +11,23 @@ class DCSModel(nn.Module):
         super().__init__()
         self.device = device
         
-        # Mock args to load the model and get normalization
+        
         args = argparse.Namespace(
-            network='WideResNet34', #WideResNet34 ResNet18
-            # --- Mimicking evaluate_nonrepeated.py args ---
-            none_random_training=True, # Must be False to trigger the correct model loading branch in utils.py
+            network='ResNet18', #WideResNet34 ResNet18
+            
+            none_random_training=True,
             rand_deform_training=False,
-            randpos_deform_training=True,  # This is the user's core method
+            randpos_deform_training=True,  # DCS
             randpos_multi_deform_training=False,
             only_adv_randpos_training=False,
             rand_path_training=False,
-            # --- Args for get_loaders ---
-            data_dir='~/datasets/CIFAR100/', # This path is just a placeholder
+            
+            data_dir='~/datasets/CIFAR100/', # placeholder
             batch_size=128,
             dataset='cifar100', #cifar10 cifar100
             worker=4,
-            # --- Other necessary args ---
-            is_n_repeat=False, # Must be True to load the correct block in ResNet18_1layer
+            
+            is_n_repeat=False,
             num_classes=100, # 10 100
             pretrain=model_path,
             # pos=0,
@@ -70,13 +70,13 @@ class DCSModel(nn.Module):
 # PRETRAINED_MODEL_PATH = '/mnt/ssd_2/yxma/DCS/ckpt/cifar10/ResNet18/ckpt/model_20240805133819.pth'
 # "/mnt/ssd_2/yxma/DCS/ckpt/cifar10/ResNet18/ckpt/model_20240805133819.pth" pure at c10
 
-# PRETRAINED_MODEL_PATH = '/mnt/ssd_2/yxma/DCS/ckpt/cifar100/ResNet18/ckpt/model_20241107183716.pth'
+PRETRAINED_MODEL_PATH = '/mnt/ssd_2/yxma/DCS/ckpt/cifar100/ResNet18/ckpt/model_20241107183716.pth'
 # ""/mnt/ssd_2/yxma/DCS/ckpt/cifar100/ResNet18/ckpt/model_20241107183716.pth"" r18dcs c100
-PRETRAINED_MODEL_PATH = '/mnt/ssd_2/yxma/DCS/ckpt/cifar100/WideResNet34/ckpt/model_20241107185520.pth'
+# PRETRAINED_MODEL_PATH = '/mnt/ssd_2/yxma/DCS/ckpt/cifar100/WideResNet34/ckpt/model_20241107185520.pth'
 # "/mnt/ssd_2/yxma/DCS/ckpt/cifar100/WideResNet34/ckpt/model_20241107185520.pth" widedcs c100
 
 
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 # The model name should follow the convention: <LastName><Year><FirstWordOfTheTitle>
 model_name = "Ma2024Adversarial"
@@ -87,7 +87,7 @@ model = DCSModel(PRETRAINED_MODEL_PATH, device)
 # Run the benchmark
 clean_acc, robust_acc = benchmark(model,
                                   model_name=model_name,
-                                  n_examples=1000,  # Using 1000 examples for a quicker test run
+                                  n_examples=10000,  # Using 1000 examples for a quicker test run
                                   dataset='cifar100',
                                   threat_model='Linf',
                                   eps=8/255,
